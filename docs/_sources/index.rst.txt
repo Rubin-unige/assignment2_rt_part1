7c8dd80c530a3e37261cd055e75b2f64c9d260e7
rubin-unige <S6558048@studenti.unige.it>
2025-02-23 12:18:26 +0100

    added docs

---
+++ docs/_sources/index.rst.txt
@@ -0,0 +1,21 @@
+.. Research Track I - Second Assignment Part I documentation master file, created by
+   sphinx-quickstart on Sun Feb 23 12:05:58 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Research Track I - Second Assignment Part I
+=======================================================================
+Action Client and Service Node for setting and retrieving target coordinates
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ docs/source/conf.py
@@ -0,0 +1,53 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Research Track I - Second Assignment Part I'
+copyright = '2025, Rubin Khadka Chhetri'
+author = 'Rubin Khadka Chhetri'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+'sphinx.ext.autodoc',
+'sphinx.ext.doctest',
+'sphinx.ext.intersphinx',
+'sphinx.ext.todo',
+'sphinx.ext.coverage',
+'sphinx.ext.mathjax',
+'sphinx.ext.ifconfig',
+'sphinx.ext.viewcode',
+'sphinx.ext.githubpages',
+"sphinx.ext.napoleon",
+'sphinx.ext.inheritance_diagram',
+'breathe'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# Extension configuration
+# -- Options for intersphinx extension ---------------------------------------
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+
+# -- Options for todo extension ----------------------------------------------
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
